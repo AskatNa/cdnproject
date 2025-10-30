13a271b6bd07f781a5244df9abcab7384badca34
Askat <231308@astanait.edu.kz>
2025-10-30 23:30:53 +0500

    changed mongo con

--- src/configs/mongoConnection.js
+++ src/configs/mongoConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
-        const uri = process.env.MONGO_URI;
+        const uri = process.env.MONGO_URI || process.env.MONGO_URL;
         if (!uri) throw new Error("MONGO_URI missing");
 
         await mongoose.connect(uri);
